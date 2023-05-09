86e91cfad5958ee8f03d27547c6c0ae23fab8173
irfanp22 <106128747+irfanp22@users.noreply.github.com>
2023-05-09 21:17:15 +0700

    fix

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -19,7 +19,7 @@ class Kelas extends Model
 
     public function listrole()
     {
-        return $this->hasMany(ListRole::class, 'class_id', 'class_id');
+        return $this->hasMany(ListRole::class, 'class_id', 'id');
     }
 
     public function presensi()
