33fe25b99ae8f3b1e114b716d8ce39f4fa1609bc
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2023-05-09 18:49:07 +0700

    commit

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -18,16 +18,16 @@ class ClassController extends Controller
     public function index($id)
     {
         $kls = Kelas::where('hashcode', $id)->get();
-        if(count($kls)>0) return view('kelas.home', ['id' => $id]);
+        if (count($kls) > 0) return view('kelas.home', ['id' => $id]);
         return redirect()->route('classes');
     }
     // getkelas is for dashboard classes / home
     public function getkelas()
     {
-        $list = ListRole::with('user')->whereIn('user_id', [Auth::user()->id])->get();
+        $list = ListRole::whereIn('user_id', [Auth::user()->id])->get();
         $kelas = [];
         foreach ($list as $li) {
-            $kelas[] = Kelas::whereIn('class_id', [$li->class_id])->get();
+            $kelas[] = Kelas::whereIn('id', [$li->class_id])->get();
         }
         // $kelas = Kelas::whereIn('class_id', [$list[0]->class_id])->get();
 
@@ -97,8 +97,7 @@ class ClassController extends Controller
         if ($cek == 'noclass') return redirect()->route('classes.join');
         else if ($cek == 'ajoin') {
             return redirect()->route('classes.home', $hash);
-        }
-        else if ($cek) {
+        } else if ($cek) {
             $clid = Kelas::where('class_code', $kode)->value('class_id');
             $rid = Roles::where('role', 'student')->value('role_id');
             $uid = Auth::id();
