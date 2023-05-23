0e001a85fcfa166fe73abd57fe74da6e142d2be4
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2023-05-23 15:36:50 +0700

    commit

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -11,10 +11,11 @@ class PresensiController extends Controller
     //
     public function store(Request $req)
     {
-        $mulai = Carbon::parse($req->mulai)->addDays(-7);
-        $akhir = Carbon::parse($req->akhir)->addDays(-7);
 
+        $httpReferer = $req->headers->get('referer');
         if ($req->setiap == "minggu") {
+            $mulai = Carbon::parse($req->mulai)->addDays(-7);
+            $akhir = Carbon::parse($req->akhir)->addDays(-7);
             for ($i = 0; $i < $req->jumlah; $i++) {
                 $presensi = new Presensi();
                 $presensi->class_id = $req->idkelas;
@@ -29,6 +30,41 @@ class PresensiController extends Controller
                 $presensi->save();
                 $presensi = null;
             }
+        } else if ($req->setiap == "hari") {
+            $mulai = Carbon::parse($req->mulai)->addDays(-1);
+            $akhir = Carbon::parse($req->akhir)->addDays(-1);
+            for ($i = 0; $i < $req->jumlah; $i++) {
+                $presensi = new Presensi();
+                $presensi->class_id = $req->idkelas;
+                $mulai =
+                    Carbon::parse($mulai)->addDays(1);
+                $presensi->timestart =
+                    $mulai;
+                $akhir =
+                    Carbon::parse($akhir)->addDays(1);
+                $presensi->timeend =
+                    $akhir;
+                $presensi->save();
+                $presensi = null;
+            }
+        } else if ($req->setiap == "bulan") {
+            $mulai = Carbon::parse($req->mulai)->addMonths(-1);
+            $akhir = Carbon::parse($req->akhir)->addMonths(-1);
+            for ($i = 0; $i < $req->jumlah; $i++) {
+                $presensi = new Presensi();
+                $presensi->class_id = $req->idkelas;
+                $mulai =
+                    Carbon::parse($mulai)->addMonths(1);
+                $presensi->timestart =
+                    $mulai;
+                $akhir =
+                    Carbon::parse($akhir)->addMonths(1);
+                $presensi->timeend =
+                    $akhir;
+                $presensi->save();
+                $presensi = null;
+            }
         }
+        return redirect($httpReferer);
     }
 }
