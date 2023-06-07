4488d3f4c5bb2a2b1bb6f1314a09da90defa4a34
Stephen Prasetya Chrismawan <stephenprasetyachrismawan27@gmail.com>
2023-06-07 11:17:02 +0700

    commit

--- resources/views/kelas/home.blade.php
+++ resources/views/kelas/home.blade.php
@@ -213,11 +213,12 @@
                 </div>
                 <!-- Modal body -->
                 <form action="#">
+                    <input type="hidden" id="id_pres">
                     <div class="grid gap-4 mb-4 sm:grid-cols-2">
                         <div>
                             <label for="tanggal"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal</label>
-                            <input type="date" name="tanggal" id="tanggal">
+                            <input type="date" name="tanggal" class="tanggal">
                         </div>
                         <div>
 
@@ -225,20 +226,20 @@
                         <div>
                             <label for="start"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dimulai</label>
-                            <input type="time" name="start" id="start"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <input type="time" name="start"
+                                class="start bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div>
                         <div>
                             <label for="end"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Diakhiri</label>
-                            <input type="time" name="end" id="end"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <input type="time" name="end"
+                                class="end bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div>
                         <div class="sm:col-span-2">
                             <label for="ket"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi</label>
-                            <textarea id="ket" rows="5" name="ket"
-                                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            <textarea rows="5" name="ket"
+                                class="kete block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="Tulis Deskripsi"></textarea>
                         </div>
                     </div>
@@ -261,12 +262,18 @@
         $('#tabelabsen').DataTable();
 
         // Menangkap klik tombol Edit dan menampilkan modal
-        $(document).on('click', '.edit-btn', function() {
+        $('.edit-btn').click(function() {
             var id = $(this).data('id');
             var tanggal = $(this).data('tanggal');
             var timestart = $(this).data('timestart');
             var timeend = $(this).data('timeend');
             var ket = $(this).data('ket');
+            console.log(timestart);
+            console.log(tanggal);
+            $('.tanggal').val(tanggal);
+            $('.start').val(timestart);
+            $('.end').val(timeend);
+            $('.kete').val(ket);
 
             $.ajax({
                 url: '/presensi/edit',
