8cf49060b253f1a5aa491123573f15d253e6f590
Stephen Prasetya Chrismawan <stephenprasetyachrismawan27@gmail.com>
2023-06-08 20:34:53 +0700

    commit

--- resources/views/laporan_presensi.blade.php
+++ resources/views/laporan_presensi.blade.php
@@ -35,19 +35,28 @@
             <article class="m-3 my-10 flex flex-col items-center justify-center prose">
                 <h2 class="h1">Data Realtime Presensi ..⏳</h2>
             </article>
-            <div class="m-1 relative overflow-x-auto flex">
+            <div class="m-1 relative overflow-x-auto flex w-screen justify-center">
 
                 <div
-                    class="max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 overflow-y-auto">
-                    <div class="flex items-center justify-between mb-4">
-                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Realtime Report</h5>
+                    class="max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 overflow-y-auto w-4/5">
+                    <div class="flex items-center justify-between mb-4 ">
+                        <span class="mx-7">
+                            <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white mx-7">Realtime
+                                Report</h5>
+                        </span>
                         <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
                             View all
                         </a>
                     </div>
                     <div class="flow-root ">
                         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700" id="square">
+                            @php
+                                $stat = 0;
+                            @endphp
                             @foreach ($data as $d)
+                                @php
+                                    $stat += 1;
+                                @endphp
                                 <li class="py-3 sm:py-4">
                                     <div class="flex items-center space-x-4">
                                         <div class="flex-shrink-0">
@@ -74,126 +83,7 @@
                                 </li>
                             @endforeach
 
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center space-x-4">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full"
-                                            src="/docs/images/people/profile-picture-1.jpg" alt="Neil image">
-                                    </div>
-                                    <div class="flex-1 min-w-0">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Neil Sims
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        Hadir
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        27/10/2004 09.40 UTC
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center space-x-4">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full"
-                                            src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
-                                    </div>
-                                    <div class="flex-1 min-w-0">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Bonnie Green
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        Hadir
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        27/10/2004 09.40 UTC
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center space-x-4">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full"
-                                            src="/docs/images/people/profile-picture-2.jpg" alt="Michael image">
-                                    </div>
-                                    <div class="flex-1 min-w-0">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Michael Gough
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        Hadir
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        27/10/2004 09.40 UTC
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center space-x-4">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full"
-                                            src="/docs/images/people/profile-picture-4.jpg" alt="Lana image">
-                                    </div>
-                                    <div class="flex-1 min-w-0">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Lana Byrd
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        Hadir
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        27/10/2004 09.40 UTC
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="pt-3 pb-0 sm:pt-4">
-                                <div class="flex items-center space-x-4">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full"
-                                            src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image">
-                                    </div>
-                                    <div class="flex-1 min-w-0">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Thomes Lean
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        Hadir
-                                    </div>
-                                    <div
-                                        class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        27/10/2004 09.40 UTC
-                                    </div>
-                                </div>
-                            </li>
+
                         </ul>
                     </div>
                 </div>
@@ -214,4 +104,6 @@
     </div>
 
 </x-app-layout>
-<script></script>
+<script>
+    window.stop();
+</script>
