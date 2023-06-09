135b3e021dd551ed72acbdd51ef9948b70d68734
Stephen Prasetya Chrismawan <stephenprasetyachrismawan27@gmail.com>
2023-06-09 08:29:00 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,27 +5,41 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
         <!--Sign in section-->
-        <div
-        class="flex flex-row items-center justify-center lg:justify-start">
-        <p class="mb-0 mr-4 text-lg">Sign in with</p>                      
-        <a href="{{ url('auth/google') }}"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" width="35" height="35"><defs><path id="A" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="B"><use xlink:href="#A"/></clipPath><g transform="matrix(.727273 0 0 .727273 -.954545 -1.45455)"><path d="M0 37V11l17 13z" clip-path="url(#B)" fill="#fbbc05"/><path d="M0 11l17 13 7-6.1L48 14V0H0z" clip-path="url(#B)" fill="#ea4335"/><path d="M0 37l30-23 7.9 1L48 0v48H0z" clip-path="url(#B)" fill="#34a853"/><path d="M48 48L17 24l-4-3 35-10z" clip-path="url(#B)" fill="#4285f4"/></g></svg>
-        </a>
-    
+
+        <div class="flex flex-row items-center justify-center lg:justify-start">
+            <p class="mb-0 mr-4 text-lg">Sign in with</p>
+            <a href="{{ url('auth/google') }}"><svg xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" width="35" height="35">
+                    <defs>
+                        <path id="A"
+                            d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
+                    </defs>
+                    <clipPath id="B">
+                        <use xlink:href="#A" />
+                    </clipPath>
+                    <g transform="matrix(.727273 0 0 .727273 -.954545 -1.45455)">
+                        <path d="M0 37V11l17 13z" clip-path="url(#B)" fill="#fbbc05" />
+                        <path d="M0 11l17 13 7-6.1L48 14V0H0z" clip-path="url(#B)" fill="#ea4335" />
+                        <path d="M0 37l30-23 7.9 1L48 0v48H0z" clip-path="url(#B)" fill="#34a853" />
+                        <path d="M48 48L17 24l-4-3 35-10z" clip-path="url(#B)" fill="#4285f4" />
+                    </g>
+                </svg>
+            </a>
+
         </div>
 
-      <!-- Separator between social media sign in and email/password sign in -->
-      <div
-        class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-        <p
-          class="mx-4 mb-0 text-center font-semibold dark:text-white">
-          Or
-        </p>
-      </div>
+        <!-- Separator between social media sign in and email/password sign in -->
+        <div
+            class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+            <p class="mx-4 mb-0 text-center font-semibold dark:text-white">
+                Or
+            </p>
+        </div>
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -50,9 +64,9 @@
 
         <div>
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('register') }}">
-                    {{ __("Don't have an account yet? Register here") }}
-                </a>
+                href="{{ route('register') }}">
+                {{ __("Don't have an account yet? Register here") }}
+            </a>
         </div>
 
         <div class="flex items-center justify-end mt-4">
